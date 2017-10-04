73524209381de2df10b860616a8f0e0c03483ee7
Misacorp <misa.jokisalo@gmail.com>
2017-10-04 11:28:38 +0300

    Rudimentary response adding

--- store.js
+++ store.js
@@ -1,37 +1,57 @@
 "use strict";
 
 const knex = require('knex')(require('./knexfile'));
+const Promise = require("bluebird");
 
 module.exports = {
   addResponse ( response ) {
-    //  Keep track of which responses were saved and which ones weren't
-    let added = [];
-    let failed = [];
+    let data = [];
 
+    //  Loop through each input
     for(let question_id in response) {
       console.log(`${question_id} : ${response[question_id]}`);
+      data.push({
+        question_id : question_id,
+        content: response[question_id]
+      });
 
-      //  Check if question_id exists in 'question' table
-      let exists = knex.select('id').from('question')
-        .where({
-          id : question_id
-        })
-        .then(() => {
-
-        });
-        //  If yes, add it to 'responses' table and added[]
-
-        //  If no, only add response it to failed[]
-
+      // Add to 'response' table if question_id exists in 'question' table
+      // INSERT INTO response (question_id, content)
+      // SELECT '2', 'Vastaus toiseen kysymykseen'
+      // FROM question
+      // WHERE question.id = '2'
+      // knex('response')
+      //   .returning('id')
+      //   .insert({question_id: question_id}, {content: response[question_id]});
     }
-    //  Return added[] and failed[]
-    let res = { added, failed };
-    return Promise.resolve();
+
+    console.log(data);
+
+    data = [
+      { question_id: '1', content: 'Vastaus 1' },
+      { question_id: '2', content: 'Vastaus 2' },
+      { question_id: '3', content: 'Vastaus 3' },
+      { question_id: '4', content: 'Vastaus 4' }
+    ];
+
+    return Promise.try(() => {
+      knex.transaction((t) => {
+        return knex('response')
+          .transacting(t)
+          .insert(data)
+          .then(t.commit)
+          .catch(t.rollback)
+      })
+      .then(() => {
+        return "success";
+      })
+      .catch((e) => {
+        throw e;
+      })
+    });
   },
 
   addQuestion ( content ) {
-    console.log(`Add question with content ${content}`);
-
     let question_id = knex('question')
       .insert({ content })
       .then((id) => {
