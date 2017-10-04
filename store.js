6d82a868703886d29749ba95e1558188dfb8fe2c
Misacorp <misa.jokisalo@gmail.com>
2017-10-04 12:32:44 +0300

    Implemented adding responses

--- store.js
+++ store.js
@@ -1,53 +1,50 @@
 "use strict";
 
 const knex = require('knex')(require('./knexfile'));
+knex.on( 'transaction', function( queryData ) {
+    console.log( queryData );
+});
 const Promise = require("bluebird");
 
 module.exports = {
   addResponse ( response ) {
-    let data = [];
-
-    //  Loop through each input
-    for(let question_id in response) {
-      console.log(`${question_id} : ${response[question_id]}`);
-      data.push({
-        question_id : question_id,
-        content: response[question_id]
-      });
-
-      // Add to 'response' table if question_id exists in 'question' table
-      // INSERT INTO response (question_id, content)
-      // SELECT '2', 'Vastaus toiseen kysymykseen'
-      // FROM question
-      // WHERE question.id = '2'
-      // knex('response')
-      //   .returning('id')
-      //   .insert({question_id: question_id}, {content: response[question_id]});
-    }
-
-    console.log(data);
-
-    data = [
-      { question_id: '1', content: 'Vastaus 1' },
-      { question_id: '2', content: 'Vastaus 2' },
-      { question_id: '3', content: 'Vastaus 3' },
-      { question_id: '4', content: 'Vastaus 4' }
-    ];
-
     return Promise.try(() => {
-      knex.transaction((t) => {
-        return knex('response')
-          .transacting(t)
-          .insert(data)
-          .then(t.commit)
-          .catch(t.rollback)
-      })
-      .then(() => {
-        return "success";
-      })
-      .catch((e) => {
-        throw e;
-      })
+      for(let question_id in response) {
+       /*  Loop through each response, adding it to the 'response' table if
+        *  its 'question_id' exists in the 'question' table.
+        * 
+        *  SQL queries in a loop isn't really a good idea, but I have yet
+        *  to figure out how to do this with one single query.
+        */
+
+        // INSERT INTO response (question_id, content)
+        // SELECT '2', 'Vastaus toiseen kysymykseen'
+        // FROM question
+        // WHERE question.id = '2'
+
+        //  Construct SELECT statement
+        let selectStatement = knex.select(
+          knex.raw('?, ?', [
+            question_id,
+            response[question_id]
+          ])
+        )
+        .from('question')
+        .where('question.id', question_id);
+
+        //  Construct INSERT statement
+        knex(
+          knex.raw(
+            '?? (??, ??)',
+             ['response', 'question_id', 'content']
+          )
+        )
+        .insert(selectStatement)
+        .then(() => {
+          //  Do something here. The very presence of this .then() function
+          //  actually makes the query do its thing.
+        });
+      }
     });
   },
 
