bcb9b928293012f41d331b4f73e498b082b10930
Misa Jokisalo <misa.jokisalo@gmail.com>
2017-10-17 22:01:42 +0300

    Fall back to local cwd if NODE_ENV_PATH is not defined

--- config/environments/production.js
+++ config/environments/production.js
@@ -3,5 +3,6 @@ const path = require('path');
 
 nconf.set('url', process.env.APP_HOSTNAME);
 
-let knex_path = path.resolve(nconf.get('NODE_ENV_PATH'), '/knexfile.js');
+let dir = nconf.get('NODE_ENV_PATH' ) ? nconf.get('NODE_ENV_PATH') : process.cwd();
+let knex_path = path.resolve( dir  + '/knexfile.js' );
 nconf.set('knexfile', knex_path);
