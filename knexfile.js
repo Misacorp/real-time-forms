dabc40dd1d470c6ee033d753a8f68664715231e7
Misa Jokisalo <misa.jokisalo@gmail.com>
2017-10-17 21:36:47 +0300

    Knexfile for Heroku

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,22 @@
 module.exports = {
-  client: 'mysql',
-  connection: {
-    user: 'vagrant',
-    password: 'vagrant',
-    database: 'realtimeforms'
+  development: {
+    client: 'mysql',
+    connection: {
+      user: 'vagrant',
+      password: 'vagrant',
+      database: 'realtimeforms'
+    },
+    debug: true
+  },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      database: process.env.JAWSDB_MARIA_URL
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
   }
 }
